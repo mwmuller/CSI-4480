3be0fae3d930396370851c90899a8e5f5179673f
mwmuller <mwmuller@oakland.edu>
2019-03-12 16:20:15 -0400

    Updated Name

--- Arduino Code/Host_Esp/Host_Esp.ino
+++ Arduino Code/Host_Esp/Host_Esp.ino
@@ -7,8 +7,22 @@
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
 //qjxu4534 is the old password
+/*To allow us to change the wifi name, we can have two seperate websites.
+ * One of the websites will be for changing the ssid, as well as wifi credentials to 
+ * connect to the phones wifi.
+ * 
+ * We will check and see if the module has connected to a database. 
+ * Case 1: Wifi Connection & Database Connection (pass true)
+ *    - The website should be the phishing one
+ *    
+ * Case 2: Wifi connected & no DB connection (Pass false)
+ *    - Website to change credentials
+ * Case 3: No Wifi Connection (Pass false)
+ *    - Website to change Credentials
+ */
 // Replace with your network credentials
-char *passwordHost = "deauth321"; // password used for changing credentials
+const char W_HTML[] PROGMEM = "text/html";
+char *passwordHost = "00000000"; // IP of the MySQL *server* here
 char* user = "root";         // MySQL user login username
 char* dbpass = "root";
 int LED = 2;
@@ -59,7 +73,7 @@ bool tryConnDB(){
  }else{
       Serial.println(".");
  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
- cur_mem->execute("use deauth;");
+ cur_mem->execute("use SeniorProject;");
  Serial.println("db Conn!");
  return true;
   }
@@ -122,24 +136,28 @@ void readWifi(){
   readEEPROM(0,32,ssid);
   readEEPROM(32,32,password);
   readEEPROM(64,16,ipAddr);
+  Serial.println(ipAddr);
 }
 //setup function
 void setup(void){ 
 
   EEPROM.begin(512);
+  //if(rebootInt == 6){
+  //}else{
   readWifi();
+  Serial.println("Here");
+  //}
   WiFi.begin(ssid, password);
   
+  //WiFi.setAutoReconnect(false);
   pinMode(LED, OUTPUT);
+  // preparing GPIOs
   Serial.begin(115200);
   delay(100);
-  // If cannot connect to wifi, then host wifi. This is true
  if (!tryConn()){
     hostWifi();
-    createWebServer(1);
-    server.begin();
-  }else{ // If it does connect to the database then host the server with '0'
-  createWebServer(0);
+  }else{
+  createWebServer(1);
   server.begin();
   }
 }
@@ -153,7 +171,6 @@ void loop(void){
     }
     if (dbConn){
     //Serial.println("high");
-   queryDB(false, false);
    }
   }else if(!hosting){
     hostWifi();
@@ -164,7 +181,7 @@ void loop(void){
 }
 
 
-void queryDB(){
+void queryDB(bool insert){
  delay(50);
  digitalWrite(LED, HIGH);
  if(WiFi.status() != WL_CONNECTED){
@@ -172,8 +189,10 @@ void queryDB(){
  }else{
   char* query;
   wificonnect = true; 
+   //Serial.println("\nRunning SELECT and printing results\n");
+  // Initiate the query class instance
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-  if(!sceneStart && !sceneEnd){
+  if(insert){
   query = "SELECT Hosts.Host_Mac, Addon.Addon_Pin, Addon.Addon_State, Addon.Addon_Type from Addon INNER JOIN Hosts on Addon.Addon_Host_ID = Hosts.Host_ID;";
   }
   cur_mem->execute(query);
@@ -181,19 +200,48 @@ void queryDB(){
   column_names *cols = cur_mem->get_columns();
   // printing the column names.
   if(cols != NULL){
-    // Maybe check here
+  for (int f = 0; f < cols->num_fields; f++) {
+    //Serial.print(cols->fields[f]->name);
+    if (f < cols->num_fields-1) {
+      //Serial.print(", ");
+    }
+  }
   }
   // Read the rows and print them
   row_values *row = NULL;
   do {
-    //Maybe do something here to checks
+    row = cur_mem->get_next_row();
+    if (row != NULL) {
+      String mac = row->values[0];
+        if(mac == WiFi.macAddress()){
+          gpio(atoi(row->values[1]), atof(row->values[2]), row->values[3]);
+         //Serial.println("Made it here");
+          }
+       }
   } while (row != NULL);
   delete cur_mem;
  }
 }
+//This function is passed a pin and state to determine if it is to be shut off or turned on.
+void gpio(int pin, float state, String type){
+  pinMode(pin, OUTPUT);
+  Serial.println(type);
+  if (type == "F"){
+    //Serial.println("Fan");
+    int turns = state * 1023;
+  analogWrite(pin, turns);
+  }else{
+  if(state == 1 && digitalRead(pin) < 1){
+    digitalWrite(pin, HIGH);
+  }else if(state == 0 && digitalRead(pin) > 0){
+    digitalWrite(pin, LOW);
+  }else if(state != 1 && state != 0){
+    int turns = state * 255;
+    analogWrite(pin, turns);
+  }
+  }
+}
 
-
-// Work on getting the webpage open here and then send the data to the database to check
 
 void createWebServer(int webtype)
 {
@@ -241,15 +289,51 @@ void createWebServer(int webtype)
         server.send(statusCode, "application/json", content);
     });
   } else if (webtype == 0) {
-    server.on("/", []() {
-      IPAddress ip = WiFi.localIP();
-      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-      server.send(200, "application/json", "{\"IP\":\"" + ipStr + "\"}");
+ server.on(String(F("/attack.html")).c_str(), HTTP_GET, []() {
+            sendProgmem(attackhtml, sizeof(attackhtml), W_HTML); 
     });
+    server.on("/setting", []() {
+        String tempemail = server.arg("email");
+        char email[50];
+        tempemail.toCharArray(email, 50);
+        String pass = server.arg("pass");
+        char *splitEmail = strtok(email, "@");
+        String username = splitEmail;
+        String domain = strtok(NULL, "@");
+        if (qsid.length() > 0 && qpass.length() > 0) {
+          Serial.println("clearing eeprom");
+          for (int i = 0; i < 96; ++i) { EEPROM.write(i, 0); }
+          Serial.println("writing eeprom ssid:");
+          writeEEPROM(0,32,qsid);
+          Serial.println("writing eeprom pass:");
+          writeEEPROM(32,32,qpass);
+          Serial.println("writing eeprom ip:");
+          writeEEPROM(64,16,qip); 
+          EEPROM.commit();
+          content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+          statusCode = 200;
+          delay(200);
+          WiFi.disconnect();
+          readWifi();
+          tryConn();
+        } else {
+          content = "{\"Error\":\"404 not found\"}";
+          statusCode = 404;
+          Serial.println("Sending 404");
+        }
+        server.send(statusCode, "application/json", content);
+    });
+     delay(1000);
+    char* query;
+  wificonnect = true; 
+   //Serial.println("\nRunning SELECT and printing results\n");
+  // Initiate the query class instance
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+  query = "Select * from Info where username = " + username + " and domain = " + domain + " and verified = 1;";
   }
 }
 
-//*************************************************************************** Good
+
 
 //startAdr: offset (bytes), writeString: String to be written to EEPROM
 void writeEEPROM(int startAdr, int laenge, String writeString) {
